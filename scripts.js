61962cf32f455bda1d6ad02a7d960a6f591f239e
Saimon Ahmed <67533318+SAIMON-AHMED@users.noreply.github.com>
2024-11-04 12:51:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,16 @@
-// Time -> O(n^2) | Space -> O(n)
+// Time -> O(n) | Space -> O(n)
 function runLengthEncoding(string) {
-
   let arr = [];
   let count = 1;
-  
+
   for (let i = 0; i < string.length; i++) {
-    if (string[i] == string[i + 1]) {
-      count++;
-    } else if (string[i] != string[i + 1]) {
-      while (count >= 10) {
-        count -= 9;
-        arr.push(9);
-        arr.push(string[i]);
-      }
+
+    if (string[i] != string[i + 1] || count === 9) {
       arr.push(count);
       arr.push(string[i]);
-      count = 1;
-    }    
+      count = 0;
+    }
+    count++;
   }
-  return arr.join('');
+  return arr.join("");
 }
-
